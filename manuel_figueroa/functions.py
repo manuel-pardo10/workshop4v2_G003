720adbcd696e5c3a5b70e66f3a2d00fdb17c2cb9
Manuel Figueroa <mfigueroap3@correo.uss.cl>
2026-03-26 14:31:00 -0300

    taller 4 corregido

---
+++ manuel_figueroa/functions.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return (a + b)
+
+
+def divide(a, b):
+    return a / b 
+
+
+def get_element(list_, index):
+    return list_[index ]
+
+
+def convert_to_integer(value):
+    return int(float(value))
